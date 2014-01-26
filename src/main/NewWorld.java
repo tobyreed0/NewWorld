b16f7a5940bd62f6aa55f04bb91e68ff6675ee8a
Toby Reed <tobyreed0@gmail.com>
2014-01-26 14:33:49 +0000

    Added a few imports =)

--- src/main/NewWorld.java
+++ src/main/NewWorld.java
@@ -6,12 +6,22 @@ import net.minecraft.item.Item;
 import reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = "NewWorld", name ="Minecraft: A New World", version = "0.0.1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
+
 public class NewWorld {
+	
+	  
+	@Instance(value = "NewWorld")
+       public static Generic instance;
+	
 	//Items
 	public static Item parchment;
 	//Blocks
